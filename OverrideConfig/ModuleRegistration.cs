d081fc592a7d284d84eafc125d0617cc4171a27a
shibayan <me@shibayan.jp>
2017-05-06 21:21:48 +0900

    initial commit

---
+++ OverrideConfig/ModuleRegistration.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+
+[assembly: PreApplicationStartMethod(typeof(OverrideConfig.ModuleRegistration), "Initialize")]
+
+namespace OverrideConfig
+{
+    public class ModuleRegistration
+    {
+        public static void Initialize()
+        {
+            HttpApplication.RegisterModule(typeof(OverrideConfigHttpModule));
+        }
+    }
+}
---
+++ OverrideConfig/OverrideConfigHttpModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace OverrideConfig
+{
+    public class OverrideConfigHttpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            var environmentVariables = Environment.GetEnvironmentVariables()
+                                                  .Cast<DictionaryEntry>()
+                                                  .ToDictionary(x => (string)x.Key, x => (string)x.Value);
+
+            foreach (var appSetting in environmentVariables.Where(x => x.Key.StartsWith("APPSETTINGS_", StringComparison.OrdinalIgnoreCase)))
+            {
+                ConfigurationManager.AppSettings[appSetting.Key.Substring(12)] = appSetting.Value;
+            }
+
+            foreach (var connectionString in environmentVariables.Where(x => x.Key.StartsWith("SQLSERVERCONNSTR_", StringComparison.OrdinalIgnoreCase)))
+            {
+                var settings = new ConnectionStringSettings
+                {
+                    Name = connectionString.Key.Substring(17),
+                    ConnectionString = connectionString.Value,
+                    ProviderName = "System.Data.SqlClient"
+                };
+
+                ConfigurationManager.ConnectionStrings.Remove(settings.Name);
+                ConfigurationManager.ConnectionStrings.Add(settings);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
